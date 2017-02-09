f930ce1c8c5e84f85c133f3651b9879d05a96e28
Robert Jones <sirbojangles495@gmail.com>
2017-02-09 11:15:02 -0500

    Modified to include the client side route for the index.html page

--- server/app.js
+++ server/app.js
@@ -4,6 +4,14 @@ const mapRouter = require('./map').router;
 
 const app = express();
 
+const fs = require('fs');
+
+const path = require('path');
+
+app.set('view engine', 'hbs');
+app.use(express.static(path.join(__dirname, '..', 'client')));
+app.use(express.static(path.join(__dirname, '..', 'node_modules')));
 app.use('/map', mapRouter);
+
 
 module.exports = app;
