b3165c4a23288d07ea74171b55171f93dfffdb4d
Robert Jones <sirbojangles495@gmail.com>
2017-02-09 17:12:31 -0500

    modified the routing for where the index.html is

--- server/app.js
+++ server/app.js
@@ -9,9 +9,10 @@ const fs = require('fs');
 const path = require('path');
 
 app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, '..', 'client')));
-app.use(express.static(path.join(__dirname, '..', 'node_modules')));
 app.use('/map', mapRouter);
+// app.use(express.static(path.join(__dirname, '..', 'client')));
+app.use(express.static(path.join(__dirname, '..', 'build')));
+app.use(express.static(path.join(__dirname, '..', 'node_modules')));
 
 
 module.exports = app;
