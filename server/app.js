a0708a98a1f20f8112215b57f49ef0474d6a55f7
Hữu Ðà Trần <huuda.tran@quoininc.com>
2017-02-10 14:46:42 -0500

    Minor fixes

--- server/app.js
+++ server/app.js
@@ -4,14 +4,11 @@ const mapRouter = require('./map').router;
 
 const app = express();
 
-const fs = require('fs');
-
 const path = require('path');
 
 app.set('view engine', 'hbs');
 app.use('/map', mapRouter);
 app.use(express.static(path.join(__dirname, '..', 'build')));
 app.use(express.static(path.join(__dirname, '..', 'node_modules')));
-
 
 module.exports = app;
