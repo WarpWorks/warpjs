5f2c13195ee802304a3a780f95931d783ae34f81
Robert Jones <sirbojangles495@gmail.com>
2017-02-10 10:17:19 -0500

    Removed commented line

--- server/app.js
+++ server/app.js
@@ -10,7 +10,6 @@ const path = require('path');
 
 app.set('view engine', 'hbs');
 app.use('/map', mapRouter);
-// app.use(express.static(path.join(__dirname, '..', 'client')));
 app.use(express.static(path.join(__dirname, '..', 'build')));
 app.use(express.static(path.join(__dirname, '..', 'node_modules')));
 
