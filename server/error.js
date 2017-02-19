7dceab9675045aab1b06ef8c61c685f81dc64f3c
Hữu Ðà Trần <huuda.tran@quoininc.com>
2017-02-19 11:12:02 -0500

    Restructured MapError

--- server/error.js
+++ server/error.js
@@ -1,4 +1,15 @@
-class I3CError extends Error {
+function rethrow(ErrorClass, message, originalError) {
+    throw new ErrorClass(message, originalError);
 }
+
+class I3CError extends Error {
+    constructor(message, originalError) {
+        super(message);
+        this.name = `I3CPortal.${this.constructor.name}`;
+        this.originalError = originalError;
+    }
+}
+
+I3CError.rethrow = rethrow;
 
 module.exports = I3CError;
