98f91b24ab1f65a3d95012efb5b3e0e7a7a8414a
Robert Jones <robert.jones@quoininc.com>
2017-05-09 11:15:06 -0400

    The preview modal will now appear to the right of the image area on mouse hover

---
+++ client/entity/lib/utilities/constants.js
@@ -0,0 +1,9 @@
+const FIGURE_CONTAINER = ".figure-container";
+const MAP_AREA_MODAL_CONTAINER = "#map-area-modal-container";
+const MAP_AREA_MODAL_FLAG = "#map-area-modal-flag";
+
+module.exports = {
+    FIGURE_CONTAINER,
+    MAP_AREA_MODAL_CONTAINER,
+    MAP_AREA_MODAL_FLAG
+};
--- client/entity/lib/utilities/image-map-hover.js
+++ client/entity/lib/utilities/image-map-hover.js
@@ -1,10 +1,56 @@
 const _ = require('lodash');
 const utils = require('./../../../utils');
 const modalTemplate = require('./../../templates/partials/map-area-modal.hbs');
+const constants = require('./constants.js');
 
 class HoverPreview {
     constructor() {
         this._cache = [];
+        this._currentImgAreaCoords = [];
+    }
+
+    getImageAreaCoord(coordinate) {
+        switch(coordinate) {
+            case 'x1':
+                return this._currentImgAreaCoords[0];
+                break;
+            case 'y1':
+                return this._currentImgAreaCoords[1];
+                break;
+            case 'x2':
+                return this._currentImgAreaCoords[2];
+                break;
+            case 'y2':
+                return this._currentImgAreaCoords[3];
+                break;
+            default:
+                return 0;
+        }
+    }
+
+    getWidthOffset() {
+        const bootstrapRowWidth = $(constants.FIGURE_CONTAINER).parent().width();
+        const imgWidth = $(constants.FIGURE_CONTAINER).children().width();
+
+        if(!$(constants.FIGURE_CONTAINER).hasClass('pull-left') && (bootstrapRowWidth - imgWidth) >= 0) {
+            return (bootstrapRowWidth - imgWidth) / 2;
+        }
+
+        return 0;
+    }
+
+    updateModalCSS(modalX, modalY, modalFlagX, modalFlagY) {
+        $(constants.MAP_AREA_MODAL_CONTAINER).css({left: `${modalX}px`, top: `${modalY}px`});
+        $(constants.MAP_AREA_MODAL_FLAG).css({left: `${modalFlagX}px`, top: `${modalFlagY}px`});
+    }
+
+    calculateModalPosition(modalHeight) {
+        const modalX1 = this.getImageAreaCoord('x2') + 30 + this.getWidthOffset();
+        const modalY1 = this.getImageAreaCoord('y2') - (this.getImageAreaCoord('y2') - this.getImageAreaCoord('y1'));
+        const modalFlagX1 = this.getImageAreaCoord('x2') + 21 + this.getWidthOffset();
+        const modalFlagY1 = (modalHeight / 2) + (this.getImageAreaCoord('y2') - (this.getImageAreaCoord('y2') - this.getImageAreaCoord('y1')));
+
+        this.updateModalCSS(modalX1, modalY1, modalFlagX1, modalFlagY1);
     }
 
     extractDataAndShowModal($, resultData) {
@@ -24,8 +70,11 @@ class HoverPreview {
             containsHTML
         };
 
-        $('#map-area-modal-container').html(modalTemplate(overViewData));
-        $('#map-area-modal-container').show();
+        $(constants.MAP_AREA_MODAL_CONTAINER).html(modalTemplate(overViewData));
+        $(constants.MAP_AREA_MODAL_CONTAINER).show();
+        $(constants.MAP_AREA_MODAL_FLAG).show();
+
+        this.calculateModalPosition($(constants.MAP_AREA_MODAL_CONTAINER).height());
     }
 
     getResults($, href, cachedResult) {
@@ -34,7 +83,7 @@ class HoverPreview {
             cachedResult.pending = false;
             cachedResult[href] = result.data;
 
-            if(cachedResult.canShowModal) {
+            if (cachedResult.canShowModal) {
                 this.extractDataAndShowModal($, result.data);
             }
         });
@@ -47,15 +96,20 @@ class HoverPreview {
     }
 
     onFocus($, event) {
+        this._currentImgAreaCoords = _.reduce($(event.currentTarget).attr('coords').split(","), (memo, value, key) => {
+            memo.push(parseInt(value));
+            return memo;
+        }, []);
+
         const href = $(event.currentTarget).data('targetHref');
 
         if (href) {
             const cachedResult = this.findCacheResultForHref(href);
 
-            if(cachedResult.length) {
+            if (cachedResult.length) {
                 cachedResult[0].canShowModal = true;
 
-                if(!cachedResult[0].pending) {
+                if (!cachedResult[0].pending) {
                     this.extractDataAndShowModal($, cachedResult[0][href]);
                 }
             } else {
@@ -75,14 +129,18 @@ class HoverPreview {
     }
 
     onBlur($, event) {
+        this._currentImgAreaCoords = [];
+        this.updateModalCSS(0, 0, 0, 0);
+
         const href = $(event.currentTarget).data('targetHref');
         const cachedResult = this.findCacheResultForHref(href);
 
-        if(cachedResult.length) {
+        if (cachedResult.length) {
             cachedResult[0].canShowModal = false;
         }
 
-        $('#map-area-modal-container').hide();
+        $(constants.MAP_AREA_MODAL_CONTAINER).hide();
+        $(constants.MAP_AREA_MODAL_FLAG).hide();
     }
 }
 
