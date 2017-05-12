7e266950d2514b7c52bba3baadb5e703eff5192b
Robert Jones <robert.jones@quoininc.com>
2017-05-12 12:22:20 -0400

    Image Area now supports circles and polygons. Modified how result data is cached. Modal now is position relative to the image area and image zone

--- client/entity/lib/utilities/image-map-hover.js
+++ client/entity/lib/utilities/image-map-hover.js
@@ -5,24 +5,18 @@ const constants = require('./constants.js');
 
 class HoverPreview {
     constructor() {
-        this._cache = [];
-        this._currentImgAreaCoords = [];
+        this._imageAreaCache = {};
+        this._resultDataCache = {};
+        this._imagePartition = null;
     }
 
-    getImageAreaCoord(coordinate) {
-        switch (coordinate) {
-            case 'x1':
-                return this._currentImgAreaCoords[0];
-            case 'y1':
-                return this._currentImgAreaCoords[1];
-            case 'x2':
-                return this._currentImgAreaCoords[2];
-            case 'y2':
-                return this._currentImgAreaCoords[3];
-            default:
-                return 0;
-        }
+    updateModalCSS(modalX, modalY, modalFlagY, flagClassName) {
+        $(constants.MAP_AREA_MODAL_CONTAINER).css({left: `${modalX}px`, top: `${modalY}px`});
+        $(constants.MAP_AREA_MODAL_FLAG).css({top: `${modalFlagY}px`});
+        $(constants.MAP_AREA_MODAL_FLAG).removeClass();
+        $(constants.MAP_AREA_MODAL_FLAG).addClass(flagClassName);
     }
+
 
     getWidthOffset() {
         const bootstrapRowWidth = $(constants.FIGURE_CONTAINER).parent().width();
@@ -35,31 +29,59 @@ class HoverPreview {
         return 0;
     }
 
-    updateModalCSS(modalX, modalY, modalFlagX, modalFlagY) {
-        $(constants.MAP_AREA_MODAL_CONTAINER).css({left: `${modalX}px`, top: `${modalY}px`});
-        $(constants.MAP_AREA_MODAL_FLAG).css({left: `${modalFlagX}px`, top: `${modalFlagY}px`});
-    }
+    generateModalPositionData(modalHeight, modalWidth, zoneName, computedCoords, bottomMiddleFlagHeight) {
+        const modalPosition = {};
+        const widthOffset = this.getWidthOffset();
+        const isZoneLeft = zoneName.indexOf('left') > -1;
+        const isZoneRight = zoneName.indexOf('right') > -1;
+        const isZoneMiddle = zoneName === 'middle';
+        const isZoneTopMiddle = zoneName === 'top-middle';
 
-    calculateModalPosition(modalHeight) {
-        const modalX1 = this.getImageAreaCoord('x2') + 30 + this.getWidthOffset();
-        const modalY1 = this.getImageAreaCoord('y2') - (this.getImageAreaCoord('y2') - this.getImageAreaCoord('y1'));
-        const modalFlagX1 = this.getImageAreaCoord('x2') + 21 + this.getWidthOffset();
-        const modalFlagY1 = (modalHeight / 2) + (this.getImageAreaCoord('y2') - (this.getImageAreaCoord('y2') - this.getImageAreaCoord('y1')));
+        if(isZoneRight || isZoneLeft) {
+            let modalAdjustment;
+            let adjustedWidthOffset;
+            let adjustedModalHeight;
 
-        this.updateModalCSS(modalX1, modalY1, modalFlagX1, modalFlagY1);
-    }
+            modalAdjustment = isZoneLeft ? computedCoords.midX - modalWidth + 20 : computedCoords.midX + 30;
+            adjustedWidthOffset = isZoneLeft && widthOffset > 0 ? widthOffset - 25 : widthOffset;
+            adjustedModalHeight = Math.round(modalHeight / 2);
 
-    extractDataAndShowModal($, resultData) {
-        let overViewData = _.filter(resultData._embedded.panels, (panel) => panel.type === "Overview");
-
-        if (overViewData[0]._embedded.overviews.length) {
-            const overview = overViewData[0]._embedded.overviews[0];
-
-            this.showModal($, overview.Heading || overview.name, overview.Content, overview.containsHTML);
+            modalPosition.left = modalAdjustment + adjustedWidthOffset;
+            modalPosition.top = computedCoords.midY - adjustedModalHeight - 10;
+            modalPosition.flagHeight = adjustedModalHeight;
+            modalPosition.flagClass = isZoneLeft ? 'modal-flag-right' : 'modal-flag-left';
+        } else if(isZoneMiddle || isZoneTopMiddle) {
+            modalPosition.left = computedCoords.midX - 100 + widthOffset;
+            modalPosition.top = computedCoords.midY - modalHeight - 40;
+            modalPosition.flagHeight = modalHeight + 20;
+            modalPosition.flagClass = 'modal-flag-bottom';
+        } else {
+            modalPosition.left = computedCoords.midX - 100 + widthOffset;
+            modalPosition.top = computedCoords.midY + 30;
+            modalPosition.flagHeight = bottomMiddleFlagHeight;
+            modalPosition.flagClass = 'modal-flag-top';
         }
+        return modalPosition;
     }
 
-    showModal($, title, content, containsHTML) {
+    updateModalPosition(imageAreaReferenceKey, modalHeight, bottomMiddleFlagHeight) {
+        const modalPositionData = this._imageAreaCache[imageAreaReferenceKey].modalPositionData;
+
+        if(!modalPositionData.modalHeight) {
+            const modalWidth = modalPositionData.modalWidth;
+            const zoneName = modalPositionData.imageAreaZoneName;
+            const computedCoords = modalPositionData.imageAreaComputedCoordinates;
+
+            modalPositionData.modalHeight = modalHeight
+            modalPositionData.modalPosition = this.generateModalPositionData(modalHeight, modalWidth, zoneName, computedCoords, bottomMiddleFlagHeight);
+        }
+
+        const modalPosition = modalPositionData.modalPosition;
+
+        this.updateModalCSS(modalPosition.left, modalPosition.top, modalPosition.flagHeight, modalPosition.flagClass);
+    }
+
+    showModal($, title, content, containsHTML, imageAreaReferenceKey) {
         const overViewData = {
             title,
             content,
@@ -68,67 +90,240 @@ class HoverPreview {
 
         $(constants.MAP_AREA_MODAL_CONTAINER).html(modalTemplate(overViewData));
         $(constants.MAP_AREA_MODAL_CONTAINER).show();
-        $(constants.MAP_AREA_MODAL_FLAG).show();
 
-        this.calculateModalPosition($(constants.MAP_AREA_MODAL_CONTAINER).height());
+        const modalHeight = $(constants.MAP_AREA_MODAL_CONTAINER).height();
+        const bottomMiddleFlagHeight = $(constants.MAP_AREA_MODAL_FLAG).css('top');
+
+        this.updateModalPosition(imageAreaReferenceKey, modalHeight, bottomMiddleFlagHeight);
     }
 
-    getResults($, href) {
-        utils.getCurrentPageHAL($, href)
-        .then((result) => {
-            this._cache[href].pending = false;
-            this._cache[href].result = result.data;
+    extractDataAndShowModal($, resultData, imageAreaReferenceKey) {
+        let overViewData = _.filter(resultData._embedded.panels, (panel) => panel.type === "Overview");
 
-            if (this._cache[href].canShowModal) {
-                this.extractDataAndShowModal($, result.data);
+        if (overViewData[0]._embedded.overviews.length) {
+            const overview = overViewData[0]._embedded.overviews[0];
+
+            this.showModal($, overview.Heading || overview.name, overview.Content, overview.containsHTML, imageAreaReferenceKey);
+        }
+    }
+
+    getResults($, href, cacheReferenceKey) {
+        if(this._resultDataCache[href]) {
+            this._imageAreaCache[cacheReferenceKey].result = this._resultDataCache[href];
+            this._imageAreaCache[cacheReferenceKey].pending = false;
+
+            if(this._imageAreaCache[cacheReferenceKey].canShowModal) {
+                this.extractDataAndShowModal($, this._resultDataCache[href], cacheReferenceKey);
             }
+        } else {
+            utils.getCurrentPageHAL($, href)
+            .then((result) => {
+                this._resultDataCache[href] = result.data;
+                this._imageAreaCache[cacheReferenceKey].result = this._resultDataCache[href];
+                this._imageAreaCache[cacheReferenceKey].pending = false;
+
+                if (this._imageAreaCache[cacheReferenceKey].canShowModal) {
+                    this.extractDataAndShowModal($, this._resultDataCache[href], cacheReferenceKey);
+                }
+            });
+        }
+    }
+
+    getImagePartition(xCoord, yCoord) {
+        const filtered = _.filter(this._imagePartition, (partition) => {
+            return xCoord >= partition.minX && xCoord <= partition.maxX &&
+            yCoord >= partition.minY && yCoord <= partition.maxY
         });
+
+        return filtered.length === 1 ? filtered[0].name : "";
+    }
+
+    sortCoordinatesAscending(a, b) {
+        if(a < b) {
+            return -1;
+        } else if(a > b) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    getCoordinatesByAxis(array, cardinalAxis) {
+        return _.reduce(array, (memo, value, index) => {
+            if(cardinalAxis === 'x' && !(index % 2)) {
+                memo.push(value);
+            } else if(cardinalAxis === 'y' && index % 2) {
+                memo.push(value);
+            }
+            return memo;
+        }, []);
+    }
+
+    getImageMapCenterCoordinates(shape, formatedCoordinateArray) {
+        let lowerBoundX = 0;
+        let upperBoundX = 0;
+        let lowerBoundY = 0;
+        let upperBoundY = 0;
+        let midX = 0;
+        let midY = 0;
+
+        if(shape === "circle") {
+            lowerBoundX = formatedCoordinateArray[0] - formatedCoordinateArray[2];
+            upperBoundX = formatedCoordinateArray[0] + formatedCoordinateArray[2];
+            lowerBoundY = formatedCoordinateArray[1] - formatedCoordinateArray[2];
+            upperBoundY = formatedCoordinateArray[1] + formatedCoordinateArray[2];
+            midX = formatedCoordinateArray[0];
+            midY = formatedCoordinateArray[1];
+        } else if (shape === "poly") {
+            const xValues = this.getCoordinatesByAxis(formatedCoordinateArray, 'x').sort(this.sortCoordinatesAscending);
+            const yValues = this.getCoordinatesByAxis(formatedCoordinateArray, 'y').sort(this.sortCoordinatesAscending);
+
+            lowerBoundX = xValues[0];
+            upperBoundX = xValues[xValues.length - 1];
+            lowerBoundY = yValues[0];
+            upperBoundY = yValues[yValues.length - 1];
+            midX = Math.round((lowerBoundX + upperBoundX) / 2);
+            midY = Math.round((lowerBoundY + upperBoundY) / 2);
+        } else {
+            lowerBoundX = formatedCoordinateArray[0];
+            upperBoundX = formatedCoordinateArray[2];
+            lowerBoundY = formatedCoordinateArray[1];
+            upperBoundY = formatedCoordinateArray[3];
+            midX = Math.round((lowerBoundX + upperBoundX) / 2);
+            midY = Math.round((lowerBoundY + upperBoundY) / 2);
+        }
+
+        return {
+            midX: midX,
+            midY: midY,
+            minX: lowerBoundX,
+            maxX: upperBoundX,
+            minY: lowerBoundY,
+            maxY: upperBoundY
+        }
+    }
+
+    getFormattedCoordinateList(coordinateList) {
+        return _.reduce(coordinateList.split(","), (memo, value) => {
+            memo.push(parseInt(value, 10));
+            return memo;
+        }, []);
+    }
+
+    createImageAreaCacheObject(modalWidth, areaShape, imageAreaCoordinates) {
+        const formattedCoordinates = this.getFormattedCoordinateList(imageAreaCoordinates);
+        const computedCoordinates = this.getImageMapCenterCoordinates(areaShape, formattedCoordinates);
+        const zoneName = this.getImagePartition(computedCoordinates.midX, computedCoordinates.midY);
+        const modalPositionObject = {};
+
+        return {
+            pending: true,
+            canShowModal: true,
+            result: null,
+            modalPositionData: {
+                imageAreaShape: areaShape,
+                imageAreaCoordinates: formattedCoordinates,
+                imageAreaComputedCoordinates: computedCoordinates,
+                imageAreaZoneName: zoneName,
+                modalHeight: null,
+                modalWidth: modalWidth,
+                modalPosition: modalPositionObject
+            }
+        };
+    }
+
+    generatePartitionObject(partitionName, minX, minY, maxX, maxY) {
+        return {
+            'name': partitionName,
+            'minX': minX,
+            'maxX': maxX,
+            'minY': minY,
+            'maxY': maxY,
+            'midX': Math.round((minX + maxX) / 2),
+            'midY': Math.round((minY + minY) / 2)
+
+        };
+    }
+
+    generateImagePartitions(width, height) {
+        const partitionX = Math.floor(width / 3);
+        const partitionY = Math.floor(height / 3);
+
+        const q1MinX = 0;
+        const q1MaxX = partitionX;
+
+        const q2MinX = q1MaxX+1;
+        const q2MaxX = q1MaxX*2;
+
+        const q3MinX = q2MaxX+1;
+        const q3MaxX = width;
+
+        const q1MinY = 0;
+        const q1MaxY = partitionY;
+
+        const q2MinY = q1MaxY+1;
+        const q2MaxY = q1MaxY*2;
+
+        const q3MinY = q2MaxY+1;
+        const q3MaxY = height;
+
+        this._imagePartition = [];
+
+        this._imagePartition.push(this.generatePartitionObject("top-left", q1MinX, q1MinY, q1MaxX, q1MaxY));
+        this._imagePartition.push(this.generatePartitionObject("top-middle", q2MinX, q1MinY, q2MaxX, q1MaxY));
+        this._imagePartition.push(this.generatePartitionObject("top-right", q3MinX, q1MinY, q3MaxX, q1MaxY));
+        this._imagePartition.push(this.generatePartitionObject("middle-left", q1MinX, q2MinY, q1MaxX, q2MaxY));
+        this._imagePartition.push(this.generatePartitionObject("middle", q2MinX, q2MinY, q2MaxX, q2MaxY));
+        this._imagePartition.push(this.generatePartitionObject("middle-right", q3MinX, q2MinY, q3MaxX, q2MaxY));
+        this._imagePartition.push(this.generatePartitionObject("bottom-left", q1MinX, q3MinY, q1MaxX, q3MaxY));
+        this._imagePartition.push(this.generatePartitionObject("bottom-middle", q2MinX, q3MinY, q2MaxX, q3MaxY));
+        this._imagePartition.push(this.generatePartitionObject("bottom-right", q3MinX, q3MinY, q3MaxX, q3MaxY));
     }
 
     onFocus($, event) {
-        this._currentImgAreaCoords = _.reduce($(event.currentTarget).attr('coords').split(","), (memo, value, key) => {
-            memo.push(parseInt(value));
-            return memo;
-        }, []);
+        if(!this._imagePartition) {
+            const imgHeight = Math.round($(constants.FIGURE_CONTAINER).children().height());
+            const imgWidth = Math.round($(constants.FIGURE_CONTAINER).children().width());
+
+            this.generateImagePartitions(imgWidth, imgHeight);
+        }
 
         const href = $(event.currentTarget).data('targetHref');
+        const imageAreaShape = $(event.currentTarget).attr('shape');
+        const coords = $(event.currentTarget).attr('coords');
+        const modalWidth = parseInt($(constants.MAP_AREA_MODAL_CONTAINER).css('width').split('px'),10);
+        const referenceKey = `${href}+${coords}`;
+        const cachedImageArea = this._imageAreaCache[referenceKey];
 
         if (href) {
-            const cachedResult = this._cache[href];
+            if (cachedImageArea) {
+                cachedImageArea.canShowModal = true;
 
-            if (cachedResult) {
-                cachedResult.canShowModal = true;
-
-                if (!cachedResult.pending) {
-                    this.extractDataAndShowModal($, cachedResult.result);
+                if (!cachedImageArea.pending) {
+                    this.extractDataAndShowModal($, cachedImageArea.result, referenceKey);
                 }
             } else {
-                const objectToCache = {
-                    pending: true,
-                    canShowModal: true,
-                    result: null
-                };
-
-                this._cache[href] = objectToCache;
-                this.getResults($, href);
+                this._imageAreaCache[referenceKey] = this.createImageAreaCacheObject(modalWidth, imageAreaShape, coords);
+                this.getResults($, href, referenceKey);
             }
         } else {
-            this.showModal($, $(event.currentTarget).data('previewTitle'));
+            if (!cachedImageArea) {
+                this._imageAreaCache[referenceKey] = this.createImageAreaCacheObject(modalWidth, imageAreaShape, coords);
+            }
+            this.showModal($, $(event.currentTarget).data('previewTitle'), null, null, referenceKey);
         }
     }
 
     onBlur($, event) {
-        this._currentImgAreaCoords = [];
-        this.updateModalCSS(0, 0, 0, 0);
-
         const href = $(event.currentTarget).data('targetHref');
+        const coords = $(event.currentTarget).attr('coords');
+        const referenceKey = `${href}+${coords}`;
 
-        if (this._cache[href]) {
-            this._cache[href].canShowModal = false;
+        if (this._imageAreaCache[referenceKey]) {
+            this._imageAreaCache[referenceKey].canShowModal = false;
         }
 
         $(constants.MAP_AREA_MODAL_CONTAINER).hide();
-        $(constants.MAP_AREA_MODAL_FLAG).hide();
     }
 }
 
