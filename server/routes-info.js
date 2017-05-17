62aeaa0a91d2f37a6b0ddc7a7b4c7c67891c4a32
Hữu Ðà Trần <huuda.tran@quoininc.com>
2017-05-17 14:33:33 -0400

    Added restricted access to site

--- server/routes-info.js
+++ server/routes-info.js
@@ -3,7 +3,6 @@ const RoutesInfo = require('@quoin/expressjs-routes-info');
 const entityRoutesInfo = require('./entity').routesInfo;
 const homepageRoutesInfo = require('./homepage').routesInfo;
 const mapRoutesInfo = require('./map').routesInfo;
-const session = require('./session');
 
 // const adminRouter = require('./admin').router;
 
@@ -13,11 +12,6 @@ module.exports = (subPath, baseUrl) => {
     routesInfo.use(homepageRoutesInfo('/', baseUrl));
     routesInfo.use(mapRoutesInfo('/map', baseUrl));
     routesInfo.use(entityRoutesInfo('/entity', baseUrl));
-    routesInfo.use(session.routesInfo('/session', baseUrl));
-
-    /// / TODO: Change this to use HeadStart
-    // router.use(pathInfo(pathInfo.ADMIN), session.middlewares.requiresI3cUser.bind(null, []), adminRouter);
-    // router.use(pathInfo(pathInfo.CONTENT), session.middlewares.requiresI3cUser.bind(null, []), adminRouter);
 
     return routesInfo;
 };
