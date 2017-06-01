c4e6de1fd447bbd3c708ec57006eb73d17fa24b8
Robert Jones <robert.jones@quoininc.com>
2017-06-01 09:26:23 -0400

    Added the controll for the tiny mce modal. Added the partial to the _appLayout partial

--- public/app/WarpJSClientLib.js
+++ public/app/WarpJSClientLib.js
@@ -1920,6 +1920,44 @@ WarpBasicPropertyPanelItem.prototype.updateModelWithDataFromView = function(call
     }.bind(this));
 }
 
+WarpBasicPropertyPanelItem.prototype.createTinyMCE = function(entityID) {
+
+    console.log("hey I got in this method");
+    const contentModal = $(`.container #content-modal`);
+
+    $(`.container #content-modal .modal-body`).html(`<textarea id="content-${entityID}"></textarea>`);
+    tinyMCE.init({
+        selector: `#content-${entityID}`,
+        height: 200,
+        menubar: false,
+        elementpath: false,
+        toolbar: 'bold italic linkbutton',
+        setup: function (editor) {
+            function updateContent(content) {
+                content.forEach((link) => {
+                    editor.insertContent(link);
+                });
+            }
+            editor.addButton('linkbutton', {
+                text: 'Link',
+                icon: false,
+                onclick: function () {
+                    console.log("got in the tiny mce");
+                }
+            })
+        },
+        content_css: '//www.tinymce.com/css/codepen.min.css'
+    });
+
+
+    $(`.container #content-modal`).modal("show");
+}
+
+WarpBasicPropertyPanelItem.prototype.createContentModal = function(entityID) {
+    return `
+        `;
+}
+
 WarpBasicPropertyPanelItem.prototype.createViews = function()
 {
     if (this.propertyType !== "text") {
@@ -1956,12 +1994,19 @@ WarpBasicPropertyPanelItem.prototype.createViews = function()
         var textDiv = $('<div></div>');
         textDiv.prop('class', 'col-sm-10');
 
+        var button = $('<button></button>');
+        button.prop('type', 'button');
+        button.prop('class', 'btn btn-primary');
+        button.prop('text', "View");
+        button.on('click', this.createTinyMCE.bind(null, this.globalID()));
+
         var input = $('<textarea></textarea>');
         input.prop('class', 'form-control');
         input.prop('id', this.globalID());
 
         form.append(formGroup);
         formGroup.append(label);
+        formGroup.append(button);
         formGroup.append(textDiv);
         textDiv.append(input);
 
