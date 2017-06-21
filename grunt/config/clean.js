5059c41f303b173c3b153c73324d637b5898671a
Hữu Ðà Trần <huuda.tran@quoininc.com>
2017-06-21 16:02:44 -0400

    Adjust grunt clean

--- grunt/config/clean.js
+++ grunt/config/clean.js
@@ -11,7 +11,17 @@ module.exports = {
     },
     public: {
         src: [
-            'public/app/*.js.map'
+            'public/app/*.js.map',
+            'public/app/content.js',
+            'public/app/vendor.js',
+            'public/libs/fonts',
+            'public/libs/bootstrap/bootstrap*.css',
+            'public/libs/bootstrap/bootstrap*.map'
+        ]
+    },
+    nyc: {
+        src: [
+            '.nyc_output'
         ]
     }
 };
