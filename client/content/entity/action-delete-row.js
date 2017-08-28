e3ef5366783fedc2fe16ede8a28d17021bf8d09f
Hữu Ðà Trần <huuda.tran@quoininc.com>
2017-08-27 23:08:56 -0400

    Quickly update counters when deleting. Does not take care of pagination yet.

--- client/content/entity/action-delete-row.js
+++ client/content/entity/action-delete-row.js
@@ -7,13 +7,30 @@ module.exports = ($, instanceDoc) => {
             url: $(this).data('warpjsUrl')
         };
 
-        return Promise.resolve()
+        // Immediate UI feedback
+
+        // Updating table counters
+        const paginationTable = $(this).closest('table').siblings('.table-pagination');
+        const pageStart = parseInt($('.table-pagination-page-start', paginationTable).text(), 10);
+        const pageEnd = parseInt($('.table-pagination-page-end', paginationTable).text(), 10);
+        $('.table-pagination-page-end', paginationTable).text(pageEnd - 1);
+        const pageTotal = parseInt($('.table-pagination-total-size', paginationTable).text(), 10);
+        $('.table-pagination-total-size', paginationTable).text(pageTotal - 1);
+        if (pageEnd === pageStart) {
+            // We deleted the last element on the current page.
+            if (pageStart === 1) {
+                // We are on the first page.
+                $('.table-pagination-page-start', paginationTable).text(0);
+            } else {
+                console.log("TODO: go to previous page if possible.");
+            }
+        }
+        $(this).closest('tr').remove();
+
+        // async server delete.
+        Promise.resolve()
             .then(() => $.ajax(ajaxOptions))
-            .then(() => {
-                $(this).closest('tr').remove();
-                // TODO: One row removed, do we need to go fetch more data to
-                // fill it up? Need to update the values of the navigation.
-            })
-            .catch((err) => console.log("Error delete-row:", err));
+            // TODO: Give UI feedback on error
+            .catch((err) => console.log("Error delete-row on server:", err));
     });
 };
