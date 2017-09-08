9bafcbb5c94b650fccd23fdf3ce24572ee5ac643
Hữu Ðà Trần <huuda.tran@quoininc.com>
2017-09-08 14:56:47 -0400

    Enabling session manager

--- server/portal/instance/extract-write-access.js
+++ server/portal/instance/extract-write-access.js
@@ -1,19 +1,17 @@
-// const debug = require('debug')('W2:portal:entity:extractWriteAccess');
 const Promise = require('bluebird');
 const RoutesInfo = require('@quoin/expressjs-routes-info');
 
 const config = require('./../config');
 
-module.exports = (req, responseResource, persistence, hsEntity, instance) => {
+module.exports = (req, responseResource, persistence, entity, instance) => {
     return Promise.resolve()
-        .then(() => hsEntity.canBeEditedBy(persistence, instance, req.i3cUser))
+        .then(() => entity.canBeEditedBy(persistence, instance, req.warpjsUser))
         .then((canWrite) => {
             if (canWrite) {
                 responseResource.link('edit', {
                     href: RoutesInfo.expand('W2:content:instance', {
                         domain: config.domainName,
                         type: instance.type,
-                        oid: instance.id, // FIXME: debug
                         id: instance.id
                     }),
                     title: `Edit "${instance.Name}"`
