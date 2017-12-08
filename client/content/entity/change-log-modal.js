f6cb6c34cdbb61effbb12ea516977a6d7ba421bf
Hữu Ðà Trần <huuda.tran@quoininc.com>
2017-12-08 15:59:55 -0500

    Update moment ago to fix issue #46.

--- client/content/entity/change-log-modal.js
+++ client/content/entity/change-log-modal.js
@@ -1,5 +1,12 @@
+const moment = require('moment');
+
 module.exports = ($, instanceDoc) => {
     instanceDoc.on('click', '[data-warpjs-action="change-logs"]', function() {
+        // Make sure to update the time before displaying.
+        $('[data-warpjs-modal="change-logs"] .warpjs-change-log-date-value', instanceDoc).each((index, element) => {
+            const timestamp = $(element).data('warpjsTimestamp');
+            $(element).text(moment(timestamp).fromNow());
+        });
         $('[data-warpjs-modal="change-logs"]', instanceDoc).modal('show');
     });
 };
