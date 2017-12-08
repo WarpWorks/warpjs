71e507147ba8f8844bdc7e959671fe3229bf3909
Hữu Ðà Trần <huuda.tran@quoininc.com>
2017-12-08 13:43:24 -0500

    Adding cache for domain file

--- client/portal/entity/lib/index.js
+++ client/portal/entity/lib/index.js
@@ -6,41 +6,35 @@ const template = require("./../templates/index.hbs");
 
 const errorTemplate = warpjsUtils.requirePartial('error-portal');
 
-(($) => $(document).ready(() => {
-    const loader = warpjsUtils.toast.loading($, "Page loading");
-    warpjsUtils.getCurrentPageHAL($)
-        .then(
-            (result) => {
-                warpjsUtils.toast.close($, loader);
-                if (result.error) {
-                    $('#warpjs-content-placeholder').html(errorTemplate(result.data));
-                } else {
-                    const content = template(result.data);
-                    if (result.data && result.data.Name) {
-                        document.title = result.data.Name;
-                    }
-
-                    if (result.data._embedded && result.data._embedded.previews) {
-                        result.data._embedded.previews.forEach((preview) => {
-                            warpjsUtils.cache.set(preview._links.preview.href, {
-                                title: preview.title,
-                                content: preview.content
-                            });
-                        });
-                    }
-
-                    $('#warpjs-content-placeholder').html(content);
-                    warpjsUtils.documentReady($);
-
-                    preview($);
+(($) => $(document).ready(() => warpjsUtils.getCurrentPageHAL($)
+    .then(
+        (result) => {
+            if (result.error) {
+                $(warpjsUtils.constants.CONTENT_PLACEHOLDER).html(errorTemplate(result.data));
+            } else {
+                const content = template(result.data);
+                if (result.data && result.data.Name) {
+                    document.title = result.data.Name;
                 }
-            },
-            (err) => {
-                warpjsUtils.toast.close($, loader);
-                warpjsUtils.toast.error($, err.message, "Error contacting server");
+
+                if (result.data._embedded && result.data._embedded.previews) {
+                    result.data._embedded.previews.forEach((preview) => {
+                        warpjsUtils.cache.set(preview._links.preview.href, {
+                            title: preview.title,
+                            content: preview.content
+                        });
+                    });
+                }
+
+                $(warpjsUtils.constants.CONTENT_PLACEHOLDER).html(content);
+                warpjsUtils.documentReady($);
+
+                preview($);
             }
-        )
-        .catch((err) => {
-            warpjsUtils.toast.error($, err.message, "Error processing response");
-        });
-}))(jQuery);
+        },
+        (err) => warpjsUtils.toast.error($, err.message, "Error contacting server")
+    )
+    .catch((err) => {
+        warpjsUtils.toast.error($, err.message, "Error processing response");
+    })
+))(jQuery);
