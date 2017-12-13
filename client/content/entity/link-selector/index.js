80f26bdf12bae838a114137e824443c98aaf276b
Hữu Ðà Trần <huuda.tran@quoininc.com>
2017-12-13 11:20:01 -0500

    Minor whitespaces

--- client/content/entity/link-selector/index.js
+++ client/content/entity/link-selector/index.js
@@ -9,30 +9,29 @@ const saveSeletedEntities = require('./save-selected-entities');
 const selectOnChange = require('./select-on-change');
 const template = require('./template.hbs');
 
-module.exports = ($, instanceDoc) => {
-    return Promise.resolve()
-        .then(() => {
-            if (!$(`.${constants.SELECTION_MODAL_CLASS}`).length) {
-                selectOnChange($, instanceDoc);
-                addSelectedEntity($, instanceDoc);
-                removeSelectedEntity($, instanceDoc);
-                saveSeletedEntities($, instanceDoc);
+module.exports = ($, instanceDoc) => Promise.resolve()
+    .then(() => {
+        if (!$(`.${constants.SELECTION_MODAL_CLASS}`).length) {
+            selectOnChange($, instanceDoc);
+            addSelectedEntity($, instanceDoc);
+            removeSelectedEntity($, instanceDoc);
+            saveSeletedEntities($, instanceDoc);
 
-                return Promise.resolve()
-                    .then(() => warpjsUtils.proxy.get($, instanceDoc.data('warpjsTypesUrl')))
-                    .then((res) => {
-                        const content = template({
-                            SELECTION_MODAL_CLASS: constants.SELECTION_MODAL_CLASS,
-                            entities: res._embedded.entities
-                        });
-                        instanceDoc.append(content);
+            return Promise.resolve()
+                .then(() => warpjsUtils.proxy.get($, instanceDoc.data('warpjsTypesUrl')))
+                .then((res) => {
+                    const content = template({
+                        SELECTION_MODAL_CLASS: constants.SELECTION_MODAL_CLASS,
+                        entities: res._embedded.entities
                     });
-            }
-        })
-        .then(() => initializeSelect($, instanceDoc))
-        .then(() => $(`.${constants.SELECTION_MODAL_CLASS}`).modal('show'))
-        .catch((err) => {
-            // TODO: Show error.
-            console.log("ERROR:", err);
-        });
-};
+                    instanceDoc.append(content);
+                });
+        }
+    })
+    .then(() => initializeSelect($, instanceDoc))
+    .then(() => $(`.${constants.SELECTION_MODAL_CLASS}`).modal('show'))
+    .catch((err) => {
+        // TODO: Show error.
+        console.log("ERROR:", err);
+    });
+;
