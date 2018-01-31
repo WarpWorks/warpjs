b9e71986b12b24959fd82b5ad47a60563620d593
Hữu Ðà Trần <huuda.tran@quoininc.com>
2018-01-31 00:22:40 -0500

    Allow remove of embedded relationship

--- server/studio/relationship/index.js
+++ server/studio/relationship/index.js
@@ -1,5 +1,7 @@
 const createRelationship = require('./create-relationship');
+const removeRelationship = require('./remove-relationship');
 
 module.exports = {
+    delete: (req, res) => removeRelationship(req, res),
     post: (req, res) => createRelationship(req, res)
 };
---
+++ server/studio/relationship/remove-relationship.js
@@ -0,0 +1,59 @@
+// const debug = require('debug')('W2:studio:remove-relationship');
+const Promise = require('bluebird');
+const warpjsUtils = require('@warp-works/warpjs-utils');
+
+const DocLevel = require('./../../../lib/doc-level');
+const utils = require('./../utils');
+const warpCore = require('./../../../lib/core');
+
+module.exports = (req, res) => {
+    const { domain, type, id, relationship } = req.params;
+    const { body } = req;
+
+    const resource = warpjsUtils.createResource(req, {
+        title: `Remove child for relationship ${domain} - ${type} - ${id} - ${relationship}`,
+        domain,
+        type,
+        id,
+        relationship
+    });
+
+    return Promise.resolve()
+        .then(() => {
+            // TODO: add logger
+        })
+        .then(() => warpCore.getPersistence())
+        .then((persistence) => Promise.resolve()
+            .then(() => utils.getInstance(persistence, type, id))
+            .then((instanceData) => Promise.resolve()
+                .then(() => {
+                    if (!instanceData || !instanceData.entity || !instanceData.instance) {
+                        throw new Error(`Unable to find '${type}/${id}'.`);
+                    }
+                })
+                .then(() => DocLevel.fromString(body.docLevel))
+                .then((docLevel) => docLevel.getData(persistence, instanceData.entity, instanceData.instance, 1))
+                .then((docLevelData) => Promise.resolve()
+                    .then(() => docLevelData.model.getTargetReferences(docLevelData.instance))
+                    .then((references) => {
+                        const first = docLevelData.docLevel.first();
+                        if (first) {
+                            const { value } = first;
+                            const indexOf = references.findIndex((ref) => ref._id === value);
+                            if (indexOf !== -1) {
+                                references.splice(indexOf, 1);
+                                return instanceData.entity.updateDocument(persistence, instanceData.instance);
+                            }
+                        }
+                    })
+                )
+            )
+            .finally(() => persistence.close())
+        )
+        .then(() => utils.sendHal(req, res, resource))
+        .catch((err) => {
+            console.error(`Error removing relationship child. err=`, err);
+            utils.sendErrorHal(req, res, resource, err);
+        })
+    ;
+};
