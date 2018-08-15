4cd833c6b3e05c1e268d8985b6d548f97690f921
Hữu Ðà Trần <huuda.tran@quoininc.com>
2018-08-15 19:29:31 -0400

    - #119: Moved popover near mouseenter coords

--- client/portal/preview/index.js
+++ client/portal/preview/index.js
@@ -1,52 +1,79 @@
 const _ = require('lodash');
+const Promise = require('bluebird');
 const warpjsUtils = require('@warp-works/warpjs-utils');
 
-const centerOfImageArea = require('./center-of-image-area');
-const position = require('./position');
+const BASE_POPOVER_OPTIONS = Object.freeze({
+    html: true,
+    placement: 'top',
+    trigger: 'hover focus'
+});
+const INITIALIZED = 'warpjsPopoverInitialized';
+const CSS = Object.freeze({
+    maxHeight: '110px',
+    overflow: 'hidden'
+});
+
+function position(popover, evt) {
+    popover.offset({
+        left: Math.max(0, evt.clientX - (popover.width() / 2)),
+        top: Math.max(0, evt.clientY - popover.height())
+    });
+}
+
+function findPopover($) {
+    const popover = $('.popover.in');
+    $('.popover-content', popover).css(CSS);
+    return popover;
+}
 
 module.exports = ($) => {
     $('[data-warpjs-action="preview"][data-warpjs-preview-url]')
-        .on('mouseenter', function() {
-            let popoverOffset;
+        .on('mouseenter', function(evt) {
+            if ($(this).data(INITIALIZED)) {
+                $(this).popover('show');
 
-            const popoverOptions = {
-                html: true,
-                placement: 'top',
-                trigger: 'hover focus',
-                title: "Loading...",
-                content: '<span class="text-warning">Please wait while loading data...</span>'
-            };
-
-            // $(this).data('toggle', 'popover');
-
-            const centerCoords = centerOfImageArea($, this);
-            if (centerCoords) {
-                // This depends on the DOM.
-                const imageOffset = $(this).closest('figure').children('img').offset();
-
-                popoverOffset = {
-                    left: centerCoords.x + Math.floor(imageOffset.left),
-                    top: centerCoords.y + Math.floor(imageOffset.top) - 15
-                };
-                $(this).popover(popoverOptions);
+                const popover = findPopover($);
+                position(popover, evt);
             } else {
-                $(this).popover(_.extend({}, popoverOptions, {placement: 'auto'}));
+                Promise.resolve()
+                    .then(() => warpjsUtils.proxy.get($, $(this).data('warpjsPreviewUrl')))
+                    .then((result) => Promise.resolve()
+                        .then(() => {
+                            // Tinymce dependent.
+                            const chunks = (result.content || '').split('<br />');
+
+                            const title = `<span class="close pull-right" data-dismiss="popover" aria-label="Close" aria-hidden="true">×</span>${result.title}`;
+                            const content = chunks[0];
+
+                            const popoverOptions = _.extend({}, BASE_POPOVER_OPTIONS, { title, content });
+
+                            $(this).popover(popoverOptions);
+                            $(this).popover('show');
+
+                            const popover = findPopover($);
+                            $('[data-dismiss="popover"]', popover).on('click', () => popover.popover('hide'));
+
+                            if ($(this).hasClass('map-hover-area')) {
+                                position(popover, evt);
+                            }
+                        })
+                    )
+                    .catch(() => {
+                        const popoverOptions = _.extend({}, BASE_POPOVER_OPTIONS, {
+                            title: "Trouble loading preview",
+                            content: `<div class="alert alert-danger">Issue loading preview page</div>`
+                        });
+
+                        $(this).popover(popoverOptions);
+                        $(this).popover('show');
+
+                        findPopover($);
+                    })
+                    .finally(() => {
+                        $(this).data(INITIALIZED, true);
+                    })
+                ;
             }
-
-            $(this).popover('show');
-
-            const popover = $('.popover.in');
-
-            warpjsUtils.proxy.get($, $(this).data('warpjsPreviewUrl'))
-                .then((result) => position($, this, popover, result, popoverOffset))
-                .then(() => {
-                    $('[data-dismiss="popover"]', popover).on('click', () => popover.popover('hide'));
-                })
-                .catch(() => {
-                    $('.popover-title', popover).html("Trouble loading preview");
-                    $('.popover-content', popover).html(`<div class="alert alert-danger">Issue loading preview page</div>`);
-                })
-            ;
         })
     ;
 };
