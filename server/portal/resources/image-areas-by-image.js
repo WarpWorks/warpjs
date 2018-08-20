493246f90ea0cf8331986ebf8f2e4b56f941ee6d
Hữu Ðà Trần <huuda.tran@quoininc.com>
2018-08-20 00:01:07 -0400

    - #117: Displaying as SVG. WIP for preview

--- server/portal/resources/image-areas-by-image.js
+++ server/portal/resources/image-areas-by-image.js
@@ -1,7 +1,34 @@
-// const debug = require('debug')('W2:portal:resources/image-areas-by-image');
+const debug = require('debug')('W2:portal:resources/image-areas-by-image');
 const Promise = require('bluebird');
 const RoutesInfo = require('@quoin/expressjs-routes-info');
 const warpjsUtils = require('@warp-works/warpjs-utils');
+
+const ImageAreaShapes = require('./../../../lib/core/image-area-shapes');
+
+function defineRect(imageAreaResource) {
+    const coords = (imageAreaResource.coords || '').split(',');
+    if (coords.length === 4) {
+        imageAreaResource.x = parseInt(coords[0], 10);
+        imageAreaResource.y = parseInt(coords[1], 10);
+        imageAreaResource.w = parseInt(coords[2], 10) - imageAreaResource.x;
+        imageAreaResource.h = parseInt(coords[3], 10) - imageAreaResource.y;
+        imageAreaResource.isRect = true;
+    } else {
+        debug(`       Invalid Rect: coords=${imageAreaResource.coords}`);
+    }
+}
+
+function defineCircle(imageAreaResource) {
+    const coords = (imageAreaResource.coords || '').split(',');
+    if (coords.length === 3) {
+        imageAreaResource.cx = parseInt(coords[0], 10);
+        imageAreaResource.cy = parseInt(coords[1], 10);
+        imageAreaResource.r = parseInt(coords[2], 10);
+        imageAreaResource.isCircle = true;
+    } else {
+        debug(`        Invalid Circle: coords=${imageAreaResource.coords}`);
+    }
+}
 
 module.exports = (persistence, image, instance) => Promise.resolve()
     .then(() => image.getRelationshipByName('Map'))
@@ -16,6 +43,22 @@ module.exports = (persistence, image, instance) => Promise.resolve()
                 shape: imageArea.Shape
             }))
             .then((imageAreaResource) => Promise.resolve()
+                .then(() => debug(`shape=`, imageAreaResource.shape))
+                .then(() => {
+                    if (imageAreaResource.shape === ImageAreaShapes.Rect) {
+                        debug(`    Is Rect`);
+                        defineRect(imageAreaResource);
+                    } else if (imageAreaResource.shape === ImageAreaShapes.Circle) {
+                        debug(`    Is Circle`);
+                        defineCircle(imageAreaResource);
+                    } else if (imageAreaResource.coords) {
+                        debug(`    Assuming Rect`);
+                        defineRect(imageAreaResource);
+                    } else {
+                        debug(`    *** TODO: Unknown shape=${imageAreaResource.shape}; coords=${imageAreaResource.coords}`);
+                    }
+                })
+
                 .then(() => relationship.getTargetEntity().getRelationshipByName('Target'))
                 .then((targetRelationship) => targetRelationship.getDocuments(persistence, imageArea))
                 .then((targets) => targets.pop())
