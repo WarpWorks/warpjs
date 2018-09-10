af203da257b989861fd555f33f1a7b00357b6b88
Hữu Ðà Trần <huuda.tran@quoininc.com>
2018-09-09 22:54:33 -0400

    - #133: Auto-open first vocabulary element

--- client/portal/vocabulary/index.js
+++ client/portal/vocabulary/index.js
@@ -10,4 +10,9 @@ module.exports = ($) => {
         $('.warpjs-vocabulary-definitions .warpjs-vocabulary-definition', vocabularyPanelItem).hide();
         $(`.warpjs-vocabulary-definitions .warpjs-vocabulary-definition[data-warpjs-letter="${$(this).data('warpjsLetter')}"]`, vocabularyPanelItem).show();
     });
+
+    // Auto-open every first letter.
+    $('.warpjs-relationship-style-Vocabulary').each((i, element) => {
+        $('.warpjs-panel-items-vocabulary .warpjs-vocabulary-letters .warpjs-vocabulary-letter:first-child', element).trigger('click');
+    });
 };
