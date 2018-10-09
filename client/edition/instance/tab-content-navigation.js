f55debfd7ef286c294287666efe01b68be0dc99f
Hữu Ðà Trần <huuda.tran@quoininc.com>
2018-10-09 14:42:08 -0400

    - Fixed tab navigation by setting 'active' correctly.

--- client/edition/instance/tab-content-navigation.js
+++ client/edition/instance/tab-content-navigation.js
@@ -1,8 +1,12 @@
 module.exports = ($) => {
     // This simulate the click on tabs because we don't have IDs to link to.
-    $('.tab-content').on('click', '.nav.nav-tabs > li', function() {
-        const tabindex = $(this).data('tabindex');
-        $(this).closest('.tab-content').children('.tab-pane').removeClass('active');
-        $(this).closest('.tab-content').children(`.tab-pane[data-tabindex="${tabindex}"]`).addClass('active');
+    $('.tab-content').on('click', '.nav.nav-tabs > li', (event) => {
+        const tabindex = $(event.currentTarget).data('tabindex');
+
+        $(event.currentTarget).closest('.nav-tabs').children().removeClass('active');
+        $(event.currentTarget).addClass('active');
+
+        $(event.currentTarget).closest('.tab-content').children('.tab-pane').removeClass('active');
+        $(event.currentTarget).closest('.tab-content').children(`.tab-pane[data-tabindex="${tabindex}"]`).addClass('active');
     });
 };
