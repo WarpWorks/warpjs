64dfcfae4b98fe296b7d0c3e7c5b642fe7e766bb
Hữu Ðà Trần <huuda.tran@quoininc.com>
2018-11-01 13:24:50 -0400

    - #181: Reworked name display logic.

--- client/portal/instance/individual-contribution/component-community.jsx
+++ client/portal/instance/individual-contribution/component-community.jsx
@@ -5,6 +5,9 @@ import errorBoundary from './../../../react-utils/error-boundary';
 import LeftRightMargin from './../../../react-utils/left-right-margin';
 import PortalContent from './../../../react-utils/portal-content';
 
+// import debug from './../../debug';
+// const log = debug('client/portal/instance/individual-contribution/component-community');
+
 const convertEntry = (entry) => {
     if (entry.isUser) {
         return <span className="user">{entry.label}</span>;
@@ -13,66 +16,86 @@ const convertEntry = (entry) => {
     }
 }
 
+const And = (props) => {
+    return <span className="warpjs-individual-contribution-names-and">and</span>;
+};
+
+const displayImages = (displayedUsers) => {
+    return (
+        <div className="warpjs-individual-contribution-images">
+        </div>
+    );
+};
+
+const displayNames = (displayedUsers, moreUsers) => {
+    // FIXME: Better logic than this hard-coded.
+    const names = [];
+
+    names.push(<span className="warpjs-individual-contribution-names-item">{displayedUsers[0].label}</span>);
+
+    if (displayedUsers.length >= 2) {
+        names.push(
+            <React.Fragment>
+                {
+                    displayedUsers.length === 2
+                        ? <React.Fragment> <And/ > </React.Fragment>
+                        : <React.Fragment>, </React.Fragment>
+                }
+                <span className="warpjs-individual-contribution-names-item">{displayedUsers[1].label}</span>
+            </React.Fragment>
+        );
+    }
+
+    if (displayedUsers.length >= 3) {
+        names.push(
+            <React.Fragment>
+                {displayedUsers.length === 3 ? ', and' : ','} <span className="warpjs-individual-contribution-names-item">{displayedUsers[2].label}</span>
+            </React.Fragment>
+        );
+    }
+
+    if (displayedUsers.length === 4) {
+        names.push(
+            <React.Fragment>
+                {moreUsers ? ',' : ', and'} <span className="warpjs-individual-contribution-names-item">{displayedUsers[3].label}</span>
+            </React.Fragment>
+        );
+    }
+
+    if (moreUsers) {
+        names.push(
+            <React.Fragment> <And /> <span className="warpjs-individual-contribution-names-more">{moreUsers} more</span></React.Fragment>
+        );
+    }
+
+    names.push(
+        <a className="warpjs-individual-contribution-see-all pull-right" href="#community">see all</a>
+    );
+
+    return names;
+};
+
+const content = (users) => {
+    const displayedUsers = users.slice(0, 4);
+    const moreUsers = users.length - displayedUsers.length;
+
+    if (displayedUsers.length) {
+        return (
+            <div className="warpjs-individual-contribution-names">
+                {displayNames(displayedUsers, moreUsers)}
+            </div>
+        );
+    } else {
+        return (
+            <Col xs={12} className="warpjs-individual-contribution-no-community">
+                No authors or contributors defined
+            </Col>
+        );
+    }
+};
+
 
 const Component = (props) => {
-    const displayedUsers = props.users.slice(0, 4);
-    const moreUsers = props.users.length - displayedUsers.length;
-
-    const entries = displayedUsers.map((user) => ({
-        label: user.label,
-        isUser: true
-    }));
-    if (moreUsers) {
-        entries.push({
-            label: `${moreUsers} more`,
-        });
-    }
-
-    const namesString = entries.map((name, index, list) => {
-        if (index === list.length - 1) {
-            // Last element.
-            if (index === 1) {
-                // Only 2 elements.
-                return (
-                    <span>
-                        and
-                        {convertEntry(name)}
-                    </span>
-                );
-            } else {
-                return (
-                    <span>
-                        , and
-                        {name.isUser ? <span className="user">{name.label}</span> : name.label}
-                    </span>
-                );
-            }
-        } else if (index) {
-            // Not the first one.
-            return (
-                <span>
-                    ,
-                    {name.isUser ? <span className="user">{name.label}</span> : name.label}
-                </span>
-            )
-        } else {
-        }
-    });
-
-    let namesString;
-    if (names.length) {
-        if (names.length > 2) {
-            if (!moreUsers) {
-                names[names.length - 1] = `and ${names[names.length - 1]}`;
-            }
-            namesString = names.join(', ');
-        } else {
-            namesString = names.join(' and ');
-        }
-    }
-
-    const moreString = moreUsers ? `and ${moreUsers} more` : '';
-
     return (
         <Row className="warpjs-individual-contribution-community">
             <LeftRightMargin />
@@ -80,18 +103,7 @@ const Component = (props) => {
             <PortalContent>
                 <Grid fluid>
                     <Row>
-                        <Col xs={12}>
-                            <div className="warpjs-individual-contribution-images">
-                            </div>
-                            <div className="warpjs-individual-contribution-names">
-                              <span className="warpjs-individual-contribution-list">
-                                  {namesString}
-                              </span>
-                              <span className="warpjs-individual-contribution-see-all">
-                                <a href="#community">see all</a>
-                              </span>
-                            </div>
-                        </Col>
+                        {content(props.users)}
                     </Row>
                 </Grid>
             </PortalContent>
--- client/react-utils/portal-content.jsx
+++ client/react-utils/portal-content.jsx
@@ -1,7 +1,7 @@
 import { Col } from 'react-bootstrap';
 
 const Component = (props) => (
-    <Col xs={12} sm={10}>
+    <Col xs={12} sm={10} {...props}>
         {props.children}
     </Col>
 );
