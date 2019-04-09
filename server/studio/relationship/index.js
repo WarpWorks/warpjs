09d9583353878aa01fd7459089448ffcb7608f1e
Hữu Ðà Trần <huuda.tran@quoininc.com>
2019-04-09 11:40:36 -0400

    - Display relationship label if available in studio relationship association.

--- server/studio/relationship/index.js
+++ server/studio/relationship/index.js
@@ -3,7 +3,7 @@ const removeRelationship = require('./remove-relationship');
 const retrieveRelationshipInfo = require('./retrieve-relationship-info');
 
 module.exports = {
-    delete: (req, res) => removeRelationship(req, res),
-    get: (req, res) => retrieveRelationshipInfo(req, res),
-    post: (req, res) => createRelationship(req, res)
+    delete: async (req, res) => removeRelationship(req, res),
+    get: async (req, res) => retrieveRelationshipInfo(req, res),
+    post: async (req, res) => createRelationship(req, res)
 };
--- server/studio/relationship/retrieve-relationship-info.js
+++ server/studio/relationship/retrieve-relationship-info.js
@@ -1,14 +1,13 @@
-// const debug = require('debug')('W2:studio:relationship/retrieve-relationship-info');
-const Promise = require('bluebird');
 const RoutesInfo = require('@quoin/expressjs-routes-info');
 const warpjsUtils = require('@warp-works/warpjs-utils');
 
 const ComplexTypes = require('./../../../lib/core/complex-types');
 const constants = require('./../constants');
+// const debug = require('./debug')('retrieve-relationship-info');
 const utils = require('./../utils');
 const warpCore = require('./../../../lib/core');
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
     const { domain, type, id, relationship } = req.params;
     const { profile } = req.query;
 
@@ -23,13 +22,14 @@ module.exports = (req, res) => {
         });
 
         warpjsUtils.wrapWith406(res, {
-            [warpjsUtils.constants.HAL_CONTENT_TYPE]: () => Promise.resolve()
-                .then(() => warpjsUtils.createResource('', {
-                    domain,
-                    type: ComplexTypes.Relationship,
-                    name: ComplexTypes.Relationship
-                }))
-                .then((resourceToEmbed) => {
+            [warpjsUtils.constants.HAL_CONTENT_TYPE]: async () => {
+                try {
+                    const resourceToEmbed = warpjsUtils.createResource('', {
+                        domain,
+                        type: ComplexTypes.Relationship,
+                        name: ComplexTypes.Relationship
+                    });
+
                     resourceToEmbed.link('instances', {
                         href: RoutesInfo.expand(constants.routes.relationship, {
                             domain,
@@ -42,9 +42,12 @@ module.exports = (req, res) => {
                     });
 
                     resource.embed('entities', resourceToEmbed);
-                })
-                .then(() => utils.sendHal(req, res, resource))
-                .catch((err) => utils.sendErrorHal(req, res, resource, err))
+
+                    await utils.sendHal(req, res, resource);
+                } catch (err) {
+                    await utils.sendErrorHal(req, res, resource, err);
+                }
+            }
         });
     } else if (profile === constants.routes.PROFILES.typeItems) {
         const resource = warpjsUtils.createResource(req, {
@@ -57,38 +60,49 @@ module.exports = (req, res) => {
         });
 
         warpjsUtils.wrapWith406(res, {
-            [warpjsUtils.constants.HAL_CONTENT_TYPE]: () => Promise.resolve()
-                .then(() => warpCore.getPersistence())
-                .then((persistence) => Promise.resolve()
-                    .then(() => warpCore.getDomainByName(domain))
-                    .then((domainModel) => domainModel.getElementByPersistenceId(id))
-                    .then((pageView) => pageView.getParentEntity())
-                    .then((entityModel) => {
-                        // FIXME: Hard-coded
-                        switch (relationship) {
-                            case 'basicProperty':
-                                return entityModel.getBasicProperties();
+            [warpjsUtils.constants.HAL_CONTENT_TYPE]: async () => {
+                const persistence = await warpCore.getPersistence();
 
-                            case 'relationship':
-                                return entityModel.getRelationships();
+                try {
+                    const domainModel = await warpCore.getDomainByName(domain);
+                    const pageView = await domainModel.getElementByPersistenceId(id);
+                    const entityModel = pageView.getParentEntity();
 
-                            case 'enumeration':
-                                return entityModel.getEnums();
+                    let elements;
 
-                            default:
-                                throw new Error(`Unknown relationship='${relationship}'.`);
-                        }
-                    })
-                    .then((elements) => elements.map((element) => warpjsUtils.createResource('', {
+                    // FIXME: Hard-coded
+                    switch (relationship) {
+                        case 'basicProperty':
+                            elements = entityModel.getBasicProperties();
+                            break;
+
+                        case 'relationship':
+                            elements = entityModel.getRelationships();
+                            break;
+
+                        case 'enumeration':
+                            elements = entityModel.getEnums();
+                            break;
+
+                        default:
+                            throw new Error(`Unknown relationship='${relationship}'.`);
+                    }
+
+                    const entities = elements.map((element) => warpjsUtils.createResource('', {
                         id: element.id,
                         type: element.type,
-                        name: element.name
-                    })))
-                    .then((entities) => resource.embed('entities', entities))
-                    .finally(() => persistence.close())
-                )
-                .then(() => utils.sendHal(req, res, resource))
-                .catch((err) => utils.sendErrorHal(req, res, resource, err))
+                        name: element.label || element.name
+                    }));
+
+                    resource.embed('entities', entities);
+
+                    await utils.sendHal(req, res, resource);
+                } catch (err) {
+                    await utils.sendErrorHal(req, res, resource, err);
+                } finally {
+                    persistence.close();
+                }
+            }
         });
     } else {
         const resource = warpjsUtils.createResource(req, {
