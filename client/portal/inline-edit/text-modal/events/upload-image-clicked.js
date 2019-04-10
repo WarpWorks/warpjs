ce470837e038c4b87e01da5c3ea6eb0cd52dc059
Matt Counts <mcounts@quoininc.com>
2019-04-10 12:10:09 -0400

    warpjs #203 update: fix double upload and modal issues

--- client/portal/inline-edit/text-modal/events/upload-image-clicked.js
+++ client/portal/inline-edit/text-modal/events/upload-image-clicked.js
@@ -1,87 +1,108 @@
-const FileUpload = require('./../../../../edition/file-upload');
+// const FileUpload = require('./../../../../edition/file-upload');
+
+const constants = require('./../../../../edition/file-upload/constants.js');
+const modalTemplate = require('./../../../../edition/file-upload/modal.hbs');
 const TITLE = "File Upload";
 
+function getModal($, instanceDoc) {
+    return $(constants.MODAL_SELECTOR, instanceDoc);
+}
+
 module.exports = ($, modal) => {
-    FileUpload.init($, modal);
+    // FileUpload.init($, modal);
 
     modal.on('click', '.image-carousel-container .warpjs-inline-edit-image-upload-button', function(e) {
+        const uploadButton = $(this);
         e.stopPropagation();
         e.preventDefault();
-        $('input[type="file"]', modal).on('change', function() {
-            $(this).closest('.form-group').removeClass('has-error');
-        });
 
-        $('[data-warpjs-action="cancel-upload"]', modal).on('click', function(e) {
-            modal.modal('hide');
-        });
+        let imageUploadModal = getModal($, $('body'));
+        if (!imageUploadModal.length) {
+            $('body').append(modalTemplate({
+                MODAL_NAME: 'file-upload',
+                title: TITLE,
+                url: uploadButton.data('warpjsUrl')
+            }));
 
-        modal.on('hidden.bs.modal', (event) => {
-            modal.remove();
-        });
+            imageUploadModal = getModal($, $('body'));
 
-        $('[data-warpjs-action="confirm-upload"]', modal).on('click', function(e) {
-            const input = $('form input[type="file"]').get(0);
-            const files = input.files;
-            console.log('confirm upload', files[0], files[0].name);
-            if (files.length) {
-                const data = new FormData();
-                data.append('file', files[0], files[0].name);
-                const url = $('form', modal).data('warpjsUrl');
+            $('input[type="file"]', imageUploadModal).on('change', function() {
+                $(this).closest('.form-group').removeClass('has-error');
+            });
 
-                Promise.resolve()
-                    .then(() => window.WarpJS.toast.loading($, "Uploading file...", TITLE))
-                    .then((toastLoading) => Promise.resolve()
-                        .then(() => $.ajax({
-                            method: 'POST',
-                            url,
-                            headers: {
-                                Accept: window.WarpJS.HAL_CONTENT_TYPE
-                            },
-                            data,
-                            processData: false,
-                            contentType: false
-                        }))
-                        .then((res) => {
-                            console.log('res', res);
-                            window.WarpJS.toast.success($, "File uploaded successfully.", TITLE);
-                            modal.modal('hide');
+            $('[data-warpjs-action="cancel-upload"]', imageUploadModal).on('click', function(e) {
+                imageUploadModal.modal('hide');
+            });
 
-                            const uploadUrl = $('[data-warpjs-action="file-upload"]').data('warpjsAddImageUrl');
-                            const docLevel = $('[data-warpjs-action="file-upload"]').data('warpjsDocLevel');
-                            const data = {
-                              "width": res.info.Width,
-                              "height": res.info.Height,
-                              "url": res._links.uploadedFile.href,
-                              "docLevel": docLevel
-                            }
+            imageUploadModal.on('hidden.bs.modal', (event) => {
+                imageUploadModal.remove();
+            });
 
-                            Promise.resolve()
-                                .then(() => $.ajax({
-                                    method: 'POST',
-                                    url: uploadUrl,
-                                    contentType: 'application/json; charset=utf-8',
-                                    data: JSON.stringify(data)
-                                }))
-                                .then((res) => {
+            $('[data-warpjs-action="confirm-upload"]').on('click', function(e) {
+                e.stopPropagation();
+                e.preventDefault();
+                const input = $('form input[type="file"]').get(0);
+                const files = input.files;
+                console.log('confirm upload', files[0], files[0].name);
+                if (files.length) {
+                    const data = new FormData();
+                    data.append('file', files[0], files[0].name);
+                    const url = $('form', imageUploadModal).data('warpjsUrl');
 
-                                })
-                            ;
-                        })
-                        .catch((err) => {
-                            // eslint-disable-next-line no-console
-                            console.error("Error upload-file:", err);
-                            window.WarpJS.toast.error($, "File upload failed!", TITLE);
-                        })
-                        .finally(() => window.WarpJS.toast.close($, toastLoading))
-                    )
-                ;
-            } else {
-                window.WarpJS.toast.warning($, "Please select a file to upload", TITLE);
-                $(input).closest('.form-group').addClass('has-error');
-            }
-        });
+                    Promise.resolve()
+                        .then(() => window.WarpJS.toast.loading($, "Uploading file...", TITLE))
+                        .then((toastLoading) => Promise.resolve()
+                            .then(() => $.ajax({
+                                method: 'POST',
+                                url,
+                                headers: {
+                                    Accept: window.WarpJS.HAL_CONTENT_TYPE
+                                },
+                                data,
+                                processData: false,
+                                contentType: false
+                            }))
+                            .then((res) => {
+                                window.WarpJS.toast.success($, "File uploaded successfully.", TITLE);
+                                imageUploadModal.modal('hide');
 
-        $('form', modal).get().forEach((form) => form.reset());
-        modal.modal('show');
+                                const uploadUrl = $('[data-warpjs-action="file-upload"]').data('warpjsAddImageUrl');
+                                const docLevel = $('[data-warpjs-action="file-upload"]').data('warpjsDocLevel');
+                                const data = {
+                                  "width": res.info.Width,
+                                  "height": res.info.Height,
+                                  "url": res._links.uploadedFile.href,
+                                  "docLevel": docLevel
+                                }
+
+                                Promise.resolve()
+                                    .then(() => $.ajax({
+                                        method: 'POST',
+                                        url: uploadUrl,
+                                        contentType: 'application/json; charset=utf-8',
+                                        data: JSON.stringify(data)
+                                    }))
+                                    .then((res) => {
+
+                                    })
+                                ;
+                            })
+                            .catch((err) => {
+                                // eslint-disable-next-line no-console
+                                console.error("Error upload-file:", err);
+                                window.WarpJS.toast.error($, "File upload failed!", TITLE);
+                            })
+                            .finally(() => window.WarpJS.toast.close($, toastLoading))
+                        )
+                    ;
+                } else {
+                    window.WarpJS.toast.warning($, "Please select a file to upload", TITLE);
+                    $(input).closest('.form-group').addClass('has-error');
+                }
+            });
+        }
+
+        $('form', imageUploadModal).get().forEach((form) => form.reset());
+        imageUploadModal.modal('show');
     });
 };
