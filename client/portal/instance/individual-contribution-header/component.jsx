bd92bdf17f1728213be06e4e345fbf2baf9d8622
Hữu Ðà Trần <huuda.tran@quoininc.com>
2019-04-17 10:45:43 -0400

    - Only add header DOM if IndividualContribution

--- client/portal/instance/individual-contribution-header/component.jsx
+++ client/portal/instance/individual-contribution-header/component.jsx
@@ -4,27 +4,33 @@ import IndividualContributionCommunity from './components/community';
 import IndividualContributionTitle from './components/title';
 import IndividualContributionHeader from './components/header';
 
-// import debug from './../../debug';
-// const log = debug('client/portal/instance/individual-contribution/component');
+// import _debug from './debug'; const debug = _debug('component');
 
 const Component = (props) => {
     const pageViews = props.page.pageViews;
     const pageView = pageViews && pageViews.length ? pageViews[0] : null;
-    const communities = pageView ? pageView.communities : [];
-    const community = communities && communities.length ? communities[0] : null;
 
-    const authors = community && community.authors ? community.authors : [];
-    const contributors = community && community.contributors ? community.contributors : [];
-    const users = [].concat(authors).concat(contributors);
+    if (pageView && pageView.isOfStyle && pageView.isOfStyle.IndividualContribution) {
+        const communities = pageView ? pageView.communities : [];
+        const community = communities && communities.length ? communities[0] : null;
 
-    return (
-        <div className="warpjs-individual-contribution-header">
-            <IndividualContributionHeader />
-            <IndividualContributionCommunity users={users} />
-            <IndividualContributionTitle name={props.page.name} />
-        </div>
-    );
+        const authors = community && community.authors ? community.authors : [];
+        const contributors = community && community.contributors ? community.contributors : [];
+        const users = [].concat(authors).concat(contributors);
+
+
+        return (
+            <div className="warpjs-individual-contribution-header">
+                <IndividualContributionHeader />
+                <IndividualContributionCommunity users={users} />
+                <IndividualContributionTitle name={props.page.name} />
+            </div>
+        );
+    } else {
+        return null;
+    }
 };
+
 Component.displayName = 'IndividualContribution';
 
 Component.propTypes = {
---
+++ client/portal/instance/individual-contribution-header/debug.js
@@ -0,0 +1,3 @@
+import debug from './../debug';
+
+export default (path) => debug(`individual-contribution-header/${path}`);
