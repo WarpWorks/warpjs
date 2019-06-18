932b1a761e442614ef06d3a71e687a1befea6569
Hữu Ðà Trần <huuda.tran@quoininc.com>
2019-06-18 11:39:31 -0400

    - #235: Preparing to generate PDF

--- server/portal/entity-pdf/index.js
+++ server/portal/entity-pdf/index.js
@@ -1,5 +1,5 @@
-const generatePdf = require('./generate-pdf');
+const get = require('./get');
 
 module.exports = Object.freeze({
-    get: async (req, res) => generatePdf(req, res)
+    get: async (req, res) => get(req, res)
 });
