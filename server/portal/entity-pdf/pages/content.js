d26978fb4fece6fa319481af84a46d567bba68b8
Hữu Ðà Trần <huuda.tran@quoininc.com>
2019-06-25 02:33:46 -0400

    - #235: Adding line under "H1".

--- server/portal/entity-pdf/pages/content.js
+++ server/portal/entity-pdf/pages/content.js
@@ -1,13 +1,13 @@
 // const debug = require('./debug')('content');
 const itemElement = require('./item-element');
 
-module.exports = async (documentResource) => {
+module.exports = async (documentResource, docDefinition) => {
     if (!documentResource || !documentResource._embedded || !documentResource._embedded.items) {
         return;
     }
 
     return documentResource._embedded.items.reduce(
-        (memo, item) => memo.concat(itemElement(item)),
+        (memo, item) => memo.concat(itemElement(item, docDefinition)),
         []
     );
 };
