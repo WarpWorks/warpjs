be276e77fc9cc06f3f2123c914e4a6c2f546f4bc
Matt Counts <mcounts@quoininc.com>
2019-07-12 15:44:07 -0400

    warpjs 259 pdf generator prevent ending page with heading

--- server/portal/entity-pdf/pages/content.js
+++ server/portal/entity-pdf/pages/content.js
@@ -2,7 +2,6 @@
 const itemElement = require('./item-element');
 
 module.exports = async (documentResource, docDefinition) => {
-    console.log('docDefinition:::', docDefinition);
     if (!documentResource || !documentResource._embedded || !documentResource._embedded.items) {
         return;
     }
