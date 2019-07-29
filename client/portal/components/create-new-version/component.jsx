3b9bddd35e5f0b2054d1239c0bcc723f7d0768b2
Hữu Ðà Trần <huuda.tran@quoininc.com>
2019-07-29 10:46:14 -0400

    - #150: Remove previous predecessor association.

--- client/portal/components/create-new-version/component.jsx
+++ client/portal/components/create-new-version/component.jsx
@@ -21,13 +21,12 @@ const Component = (props) => {
             </Fragment>
         );
     } else {
-        // content = (
-        //     <Fragment>
-        //         <FormControl.Static>{props.version}</FormControl.Static>
-        //         <InputGroup.Button><Button label="New version" glyph="duplicate" style="primary" onClick={props.show} /></InputGroup.Button>
-        //     </Fragment>
-        // );
-        content = <FormControl.Static>{props.version}</FormControl.Static>;
+        content = (
+            <Fragment>
+                <FormControl.Static>{props.version}</FormControl.Static>
+                <InputGroup.Button><Button label="New version" glyph="duplicate" style="primary" onClick={props.show} /></InputGroup.Button>
+            </Fragment>
+        );
     }
 
     return (
