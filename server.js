5e9a61918f6a8d3503784f33ebd8cf5132a452ff
Hữu Ðà Trần <huuda.tran@quoininc.com>
2019-08-29 14:46:58 -0400

    - Revert back fix after eslint was fixed.

--- server.js
+++ server.js
@@ -71,13 +71,11 @@ function onError(error) {
             // eslint-disable-next-line no-console
             console.error(bind + ' requires elevated privileges');
             process.exit(1);
-            break;
 
         case 'EADDRINUSE':
             // eslint-disable-next-line no-console
             console.error(bind + ' is already in use');
             process.exit(1);
-            break;
 
         default:
             throw error;
