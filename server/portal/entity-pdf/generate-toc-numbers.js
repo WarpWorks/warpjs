d0b99e0460dec827a6ae07eefac8164ddd2913cc
Matt Counts <mcounts@quoininc.com>
2019-09-05 16:12:51 -0400

    warpjs#299: toc numbers wip

--- server/portal/entity-pdf/generate-toc-numbers.js
+++ server/portal/entity-pdf/generate-toc-numbers.js
@@ -4,20 +4,20 @@ const setTocNumber = require('./set-toc-number');
 
 const generateTocNumbers = (items, parentVersion, offset = 0) => {
     if (items) {
-        let lastParentVersion = parentVersion;
+        let newParentVersion = generateTocNumber(parentVersion, 0);
         items.reduce(
             (currentCounter, item) => {
                 debug(`${parentVersion}: ${currentCounter}: item=`, item.heading);
-                if (item.heading) {
-                    const newParentVersion = setTocNumber(item, parentVersion, currentCounter);
+                if (item.heading || item.name) {
+                    newParentVersion = setTocNumber(item, parentVersion, currentCounter + offset);
+                    offset = 0;
                     debug(`    ${parentVersion}: ${currentCounter}: newParentVersion1=`, newParentVersion);
                     if (item._embedded && item._embedded.items && item._embedded.items.length) {
                         generateTocNumbers(item._embedded.items, newParentVersion, offset);
-                        offset += item._embedded.items.length;
                     }
                     return currentCounter + 1;
                 } else if (item._embedded && item._embedded.items && item._embedded.items.length) {
-                    generateTocNumbers(item._embedded.items, parentVersion, offset);
+                    generateTocNumbers(item._embedded.items, newParentVersion, currentCounter + offset);
                     offset += item._embedded.items.length;
                 } else {
                     debug(`    ${parentVersion}: ${currentCounter}: No tocNumber`);
