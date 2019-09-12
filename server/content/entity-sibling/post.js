3a3ebca0ce69cd99ecaa0d320165ae8ce89720cc
Hữu Ðà Trần <huuda.tran@quoininc.com>
2019-09-12 15:23:57 -0700

    - #316: Use parent document version for sibling creation.

--- server/content/entity-sibling/post.js
+++ server/content/entity-sibling/post.js
@@ -3,82 +3,81 @@ const Promise = require('bluebird');
 const RoutesInfo = require('@quoin/expressjs-routes-info');
 const warpjsUtils = require('@warp-works/warpjs-utils');
 
+const { DEFAULT_VERSION } = require('./../../../lib/constants');
 const { routes } = require('./../constants');
 const serverUtils = require('./../../utils');
 const utils = require('./../utils');
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
     const { domain, type, id } = req.params;
 
     const persistence = serverUtils.getPersistence(domain);
 
-    const resource = warpjsUtils.createResource(req, {
-        title: `Sibling for domain ${domain} - Type ${type} - Id ${id}`,
-        domain,
-        type,
-        id
-    });
+    try {
+        const resource = warpjsUtils.createResource(
+            req,
+            {
+                title: `Sibling for domain ${domain} - Type ${type} - Id ${id}`,
+                domain,
+                type,
+                id
+            },
+            req
+        );
 
-    return Promise.resolve()
-        .then(() => serverUtils.getEntity(domain, type))
-        .then((entity) => Promise.resolve()
-            .then(() => entity.getInstance(persistence, id))
+        const entity = await serverUtils.getEntity(domain, type);
+        const instance = await entity.getInstance(persistence, id);
 
-            .then((instance) => Promise.resolve()
-            // TODO: check Edit permissions of the parent!!!
+        const parentData = await entity.getParentData(persistence, instance);
+        const parentVersion = parentData && parentData.instance && parentData.instance.Version ? parentData.instance.Version : DEFAULT_VERSION;
 
-                // Find sibling TEMPLATE.
-                .then(() => entity.getDocuments(persistence, {
-                    parentID: instance.parentID,
-                    parentRelnID: instance.parentRelnID,
-                    Name: 'TEMPLATE'
-                }, true))
-                .then((templateDocuments) => templateDocuments && templateDocuments.length ? templateDocuments[0] : null)
-                .then((templateDocument) => {
-                    if (templateDocument) {
-                        return Promise.resolve()
-                            .then(() => entity.getDomain().getEntityByInstance(templateDocument))
-                            .then((templateEntity) => templateEntity.clone(persistence, templateDocument))
-                            .then((deepCopy) => RoutesInfo.expand(routes.instance, {
-                                domain,
-                                type: deepCopy.type,
-                                id: deepCopy.id
-                            }))
-                            .then((redirectUrl) => utils.sendRedirect(req, res, resource, redirectUrl))
-                        ;
-                    } else {
-                        return Promise.resolve()
-                            .then(() => entity.createSiblingForInstance(instance))
-                            .then((sibling) => entity.createDocument(persistence, sibling))
-                            .then((newDoc) => newDoc.id)
-                            .then((newId) => {
-                                const redirectUrl = RoutesInfo.expand(routes.instance, {
-                                    domain,
-                                    type,
-                                    id: newId
-                                });
+        // TODO: check Edit permissions of the parent!!!
 
-                                if (req.headers['x-requested-with']) {
-                                    // Was ajax call. return a resource.
-                                    resource.link('redirect', redirectUrl);
+        const templateDocuments = await entity.getDocuments(persistence, {
+            parentID: instance.parentID,
+            parentRelnID: instance.parentRelnID,
+            Name: 'TEMPLATE'
+        }, true);
 
-                                    utils.sendHal(req, res, resource);
-                                } else {
-                                    // Direct call.
-                                    res.redirect(redirectUrl);
-                                }
-                            })
-                        ;
-                    }
-                })
-            )
-        )
-        .catch((err) => {
-            // eslint-disable-next-line no-console
-            console.error("entity-sibling(): err=", err);
-            resource.message = err.message;
-            utils.sendHal(req, res, resource, 500);
-        })
-        .finally(() => persistence.close())
-    ;
+        const templateDocument = templateDocuments && templateDocuments.length ? templateDocuments[0] : null;
+
+        if (templateDocument) {
+            const templateEntity = entity.getDomain().getEntityByInstance(templateDocument);
+            const deepCopy = await templateEntity.clone(persistence, templateDocument, null, parentVersion);
+            const redirectUrl = RoutesInfo.expand(routes.instance, {
+                domain,
+                type: deepCopy.type,
+                id: deepCopy.id
+            });
+            utils.sendRedirect(req, res, resource, redirectUrl);
+        } else {
+            const sibling = await entity.createSiblingForInstance(instance);
+            sibling.Version = parentVersion;
+            const newDoc = await entity.createDocument(persistence, sibling);
+            const newId = newDoc.id;
+
+            const redirectUrl = RoutesInfo.expand(routes.instance, {
+                domain,
+                type,
+                id: newId
+            });
+
+            if (req.headers['x-requested-with']) {
+                // Was ajax call. return a resource.
+                resource.link('redirect', redirectUrl);
+
+                utils.sendHal(req, res, resource);
+            } else {
+                // Direct call.
+                res.redirect(redirectUrl);
+            }
+        }
+    } catch (err) {
+        // eslint-disable-next-line no-console
+        console.error("entity-sibling(): err=", err);
+        resource.message = err.message;
+        utils.sendHal(req, res, resource, 500);
+    } finally {
+        persistence.close();
+    }
 };
