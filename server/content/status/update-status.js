5aa39572184c86669be30f286fd5d019bec72c30
Hữu Ðà Trần <huuda.tran@quoininc.com>
2019-10-01 14:25:35 -0400

    - #324: Set all predecessors to Retired.

--- server/content/status/update-status.js
+++ server/content/status/update-status.js
@@ -1,5 +1,9 @@
+const Promise = require('bluebird');
+
+const ChangeLogs = require('@warp-works/warpjs-change-logs');
 const warpjsUtils = require('@warp-works/warpjs-utils');
 
+const DOCUMENT_STATUS = require('./../../../lib/constants/document-status');
 const Document = require('./../../../lib/core/first-class/document');
 const DocumentStatus = require('./../../../lib/core/first-class/document-status');
 const serverUtils = require('./../../utils');
@@ -30,15 +34,42 @@ module.exports = async (req, res) => {
             throw new Error(`Invalid promotion status from '${document.Status}' to '${status}'.`);
         }
 
-        document.Status = status;
+        const statusEnum = entity.getEnumByName('Status');
+        if (! statusEnum) {
+            throw new Error(`Invalid entity without 'Status'.`);
+        }
+
+        const { oldValue, newValue } = await statusEnum.setValue(document, status);
+        ChangeLogs.add(ChangeLogs.ACTIONS.UPDATE_VALUE, req.warpjsUser, document, {
+            key: 'Enum:Status',
+            oldValue,
+            newValue
+        });
+        await entity.updateDocument(persistance, document);
+
+        if (newValue === DOCUMENT_STATUS.APPROVED) {
+            const predecessorRelationship = entity.getRelationshipByName('Predecessor')
+            const domainEntity = entity.getDomain();
+            if (predecessorRelationship) {
+                const predecessors = await predecessorRelationship.recursiveGetDocuments(persistance, document);
+                await Promise.each(predecessors, async (predecessor) => {
+                    const predecessorEntity = domainEntity.getEntityByInstance(predecessor);
+                    const { oldValue, newValue } = await statusEnum.setValue(predecessor, DOCUMENT_STATUS.RETIRED);
+                    ChangeLogs.add(ChangeLogs.ACTIONS.UPDATE_VALUE, req.warpjsUser, predecessor, {
+                        key: 'Enum:Status',
+                        oldValue,
+                        newValue
+                    });
+                    await predecessorEntity.updateDocument(persistance, predecessor);
+                });
+            }
+
+        }
 
         resource.status = {
             documentStatus: await Document.computeDocumentStatus(persistance, entity, document),
             promotion: Document.statusPromotion(req, entity, document)
         };
-
-        // TODO: ChangeLogs
-        // TODO: updateDocument.
 
         utils.sendHal(req, res, resource);
     } catch (err) {
